9bbd95762046aaa6bb2594ede708b630fb484187
Egor Lyadskiy <47715067+Egor-Liadsky@users.noreply.github.com>
2022-12-18 07:40:15 +0300

    фикс даты в этапах

--- app/src/main/java/com/turtleteam/myapp/adapters/StepAdapter.kt
+++ app/src/main/java/com/turtleteam/myapp/adapters/StepAdapter.kt
@@ -1,17 +1,25 @@
 package com.turtleteam.myapp.adapters
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.turtleteam.myapp.R
 import com.turtleteam.myapp.data.model.step.Step
 import com.turtleteam.myapp.databinding.StepItemBinding
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalQueries.zone
+
 
 class StepAdapter(
     private val edit: (item: Step) -> Unit,
@@ -27,11 +35,33 @@ class StepAdapter(
         private val url: (item: String) -> Unit,
     ) :
         RecyclerView.ViewHolder(binding.root) {
+        @RequiresApi(Build.VERSION_CODES.O)
         @SuppressLint("SetTextI18n")
         fun bind(item: Step) {
+
+            val date_start = item.date_start
+            val localDate1 = LocalDateTime.parse(date_start, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+            val offset1 = OffsetDateTime.of(localDate1, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+
+            Log.e("DATE START", date_start.toString())
+
+
+            val date_end = item.date_end
+            Log.e("DATE END", offset1.toString())
+//            val offset2 = OffsetDateTime.parse(date_end, DateTimeFormatter.ofPattern("yyy-MM-dd'T'HH:mm:ss"))
+
+
+
+            //            val actual1 = OffsetDateTime.parse(date_start, DateTimeFormatter.ISO_DATE_TIME)
+//            val formatter1 = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss")
+//            val formatDateTime1 = actual1.format(formatter1)
+
+            //            val actual2 = OffsetDateTime.parse(date_end, DateTimeFormatter.ISO_DATE_TIME)
+//            val formatter2 = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss")
+            //            val formatDateTime2 = actual2.format(formatter2)
             binding.title.text = item.header
             binding.description.text = item.text
-            binding.time.text = "${item.date_start} : ${item.date_end}"
+            binding.time.text = "$date_start : $date_end"
 
             binding.itemCard.setOnLongClickListener {
                 showPopup(binding.itemCard, item)
@@ -68,6 +98,7 @@ class StepAdapter(
         return StepHolder(inflater, edit, delete, url)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: StepHolder, position: Int) {
         holder.bind(currentList[position])
     }
